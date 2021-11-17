6c4a6acf4ba61e04d607b21eb3c3a88bb23313e2
ANVA_DOMEIN\JackG <j.gommeren@anva.nl>
2021-11-17 13:42:38 +0100

    gallery completed

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -9,8 +9,10 @@ export class GalleryComponent implements OnInit {
 
   public mainPic = './assets/images/home1.jpg';
 
-  public pic1 = './assets/images/cartoon-olifant-1.jpg';
-  public pic2 = './assets/images/cartoon-olifant-2.jpg';
+  public pic1 = './assets/images/home1.jpg';
+  public pic2 = './assets/images/cartoon-olifant-1.jpg';
+  public pic3 = './assets/images/horton.jpg';
+  public pic4 = './assets/images/JanVis.jpg';
 
   constructor() { }
 
